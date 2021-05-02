3cd11f03a1cd66e8d111c1aa33c879996661b04c
Viktor Terentev <60063574+etovityusha@users.noreply.github.com>
2021-05-02 13:42:24 +0300

    displaying the static logo

--- investvis/settings.py
+++ investvis/settings.py
@@ -133,12 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_URL = '/files/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'accounts/avatars')
 
 
